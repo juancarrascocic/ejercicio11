71226dbb37f3c7d60cf6616d269576eac6337bda
Juan <jcarrasco@cic.es>
2017-07-19 18:37:25 +0200

    Añadida pagina y funcionalidad de botones

---
+++ public/javascripts/buttonFunctionality.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	var boton1 = $("<button id='boton1'>HOLA</button>");
+	var boton2 = $("<button id='boton2'>Boton2</button>");
+	$('body').append(boton1).append(boton2);
+
+	$('#boton1').on('click', function(){
+		var div = $("<div class='div_Juan'>HOLA JUAN</div>");
+		$('body').append(div);
+	})
+
+	$('#boton2').on('click', function(){
+		$('.div_Juan').remove();
+		if($('.div_Login').length==0){
+		var div = $("<div class='div_Login'> <p>Nombre: <input type='text' id = 'Nombre'/></p><p>Apellidos: <input type='text' id = 'Apellidos'/></p><button id = 'botonPost'>Aceptar</button></div>");
+		$('body').append(div);
+			}
+
+		$('#botonPost').on('click', function(){
+			var nombre = $('#Nombre').val();
+			var apellidos = $('#Apellidos').val();
+			
+		})
+		
+	})
+
+});
